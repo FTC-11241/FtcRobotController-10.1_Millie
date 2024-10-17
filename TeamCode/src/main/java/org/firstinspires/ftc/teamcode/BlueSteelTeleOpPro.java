57f221f08f5210814683d53e11c171619839122d
Caleb <325cchester@frhsd.com>
2024-10-17 15:00:21 -0400

    sigma...

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueSteelTeleOpPro.java
@@ -0,0 +1,151 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+/**
+ * This file contains a minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@TeleOp(name="Blue Steel TeleOp Pro", group="Linear Opmode")
+//@Disabled
+public class BlueSteelTeleOpPro extends LinearOpMode {
+
+    // Declare OpMode members.
+    Hardware11241 robot = new Hardware11241();   // Use Team 11241's hardware
+
+    public ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Initialize the hardware variables.
+        // * The init() method of the hardware class does all the work here
+        robot.init(hardwareMap);
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        double leftFrontPower = 0;
+        double rightFrontPower = 0;
+
+
+        while (opModeIsActive()) {
+
+            // Setup a variable for each drive wheel to save power level for telemetry
+            // POV Mode uses right stick to go forward/back, and side to side, left x stick to turn/spin.
+            // - This uses basic math to combine motions and is easier to drive straight.
+            double driveForward = gamepad1.left_stick_y;
+            double driveSideways = gamepad1.left_stick_x;
+            double turn = -gamepad1.right_stick_x;
+
+            leftFrontPower = Range.clip(driveForward - driveSideways + turn, -1, 1);
+            rightFrontPower = Range.clip(driveForward + driveSideways - turn, -1, 1);
+
+
+            /*
+            // Slows down movement when the right stick is in use, making it easier to turn
+            if((turn == 0) && (driveForward + driveSideways !=0)){
+                leftFrontPower = Range.clip(driveForward - driveSideways, -0.75, 0.75);
+                rightFrontPower = Range.clip(driveForward + driveSideways, -0.75, 0.75);
+                leftRearPower = Range.clip(driveForward + driveSideways, -0.75, 0.75);
+                rightRearPower = Range.clip(driveForward - driveSideways, -0.75, 0.75);
+            }else if((turn == 0) && (driveForward + driveSideways == 0)){
+                leftFrontPower = 0;
+                rightFrontPower = 0;
+                leftRearPower = 0;
+                rightRearPower = 0;
+            }else{
+                leftFrontPower = Range.clip(driveForward - driveSideways + turn, -0.5, 0.5);
+                rightFrontPower = Range.clip(driveForward + driveSideways - turn, -0.5, 0.5);
+                leftRearPower = Range.clip(driveForward + driveSideways + turn, -0.5, 0.5);
+                rightRearPower = Range.clip(driveForward - driveSideways - turn, -0.5, 0.5);
+            }
+            */
+            // D pad controls liftArm.
+            if (gamepad1.dpad_up) {
+                robot.liftMotor.setPower(-1);
+            }
+            if (gamepad1.dpad_down)
+                robot.liftMotor.setPower(1);
+            else
+                robot.liftMotor.setPower(0);
+
+            // right bumper controls intakeR and intakeL
+            if (gamepad1.right_bumper) {
+                robot.orient.setPosition(0.70);
+            }
+            else
+                robot.orient.setPosition(0.50);
+
+            if (gamepad1.right_bumper) {
+                robot.intake.setPosition(0.0);
+            }
+            else
+                robot.intake.setPosition(1.0);
+
+
+            // left bumper controls intakeL.
+            //if (gamepad1.left_bumper) {
+            //robot.intakeL.setPosition(0.50);
+
+
+
+            // Send calculated power to wheels
+
+            robot.leftDrive.setPower(leftFrontPower);
+            robot.rightDrive.setPower(rightFrontPower);
+
+
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            //telemetry.addData("Motors", "LF (%.2f), RF (%.2f), LR (%.2f), RR (%.2f)", leftFrontPower, rightFrontPower, leftRearPower, rightRearPower);
+            telemetry.update();
+        }
+    }
+}
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware11241.java
@@ -0,0 +1,190 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import static java.lang.Thread.sleep;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * This is NOT an opmode.
+ *
+ * This class can be used to define all the specific hardware for a single robot.
+ * In this case that robot is a Pushbot.
+ * See PushbotTeleopTank_Iterative and others classes starting with "Pushbot" for usage examples.
+ *
+ * This hardware class assumes the following device names have been configured on the robot:
+ * Note:  All names are lower case and some have single spaces between words.
+ *
+ * Motor channel:  Left Front drive motor:          "leftFrontDrive"
+ * Motor channel:  Right Front drive motor:         "rightFrontDrive"
+ * Motor channel:  Left Rear drive motor:           "leftRearDrive"
+ * Motor channel:  Right Rear drive motor:          "leftRearDrive"
+ * Motor channel:  Lift Arm:                        "liftMotor"
+ * Servo channel:  Servo to intake Left:            "intakeL"
+ * Servo channel:  Servo to intake Right:           "intakeR"
+ * Servo channel:  Servo to drop pixel:             "drop"
+ * Servo channel:  Servo paperairplane:             "paperairplane"
+ *
+ */
+public class Hardware11241{
+    /* Public OpMode members. */
+    public DcMotor  leftDrive         = null;
+    public DcMotor  rightDrive        = null;
+    public DcMotor  liftMotor         = null;
+    public DcMotor  hangMotor         = null;
+    public Servo    intake            = null;
+    public Servo    orient            = null;
+
+    public static final double MID_SERVO       =  0.50 ;
+
+    /* local OpMode members. */
+    private HardwareMap hwMap           = null;
+    private ElapsedTime period  = new ElapsedTime();
+
+    /* Constructor */
+    public Hardware11241(){
+
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+
+        // Define and Initialize Motors
+        leftDrive = hwMap.get(DcMotor.class, "leftFrontDrive");
+        rightDrive = hwMap.get(DcMotor.class, "rightFrontDrive");
+        liftMotor = hwMap.get(DcMotor.class, "liftMotor");
+        hangMotor = hwMap.get(DcMotor.class, "hangMotor");
+
+        leftDrive.setDirection(DcMotor.Direction.FORWARD);  // Set to FORWARD FOR goBILDA motors
+        rightDrive.setDirection(DcMotor.Direction.FORWARD); //// Set to "" goBILDA motors
+        liftMotor.setDirection(DcMotor.Direction.REVERSE);       // Set to "" goBILDA motors
+        hangMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        // Set all motors to zero power
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+        liftMotor.setPower(0);
+        hangMotor.setPower(0);
+
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+        leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        liftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        hangMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        // Define and initialize ALL installed servos.
+        intake = hwMap.get(Servo.class, "intakeL");
+        orient = hwMap.get(Servo.class, "intakeR");
+
+        intake.setPosition(1.0);
+        orient.setPosition(0.5);
+
+    }
+    public void driveForward(int time){
+        leftDrive.setPower(-0.55);
+        rightDrive.setPower(-0.55);
+
+        try {
+            Thread.sleep(time, 400);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+    }
+    public void turnLeft(){
+        leftDrive.setPower(0.55);
+        rightDrive.setPower(-0.55);
+
+        try {
+            Thread.sleep(355, 400);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+    }
+    public void turnRight(){
+        leftDrive.setPower(-0.55);
+        rightDrive.setPower(0.55);
+        try {
+            Thread.sleep(355, 400);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+    }
+    public void driveLeft(int time){
+        leftDrive.setPower(-0.55);
+        rightDrive.setPower(0);
+        try {
+            Thread.sleep(time, 400);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+    }
+    public void driveRight(int time){
+        leftDrive.setPower(0);
+        rightDrive.setPower(-0.55);
+        try {
+            Thread.sleep(time, 400);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+    }
+    public void driveBackwards(int time){
+        leftDrive.setPower(0.55);
+        rightDrive.setPower(0.55);        try {
+            Thread.sleep(time, 400);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+    }
+}
